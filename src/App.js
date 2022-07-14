dc970c1b6e82d33542af9dce0d97c1c6699db964
Sumeet Wagh <sumeetwagh70@gmail.com>
2022-07-14 12:21:53 +0530

    from url

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
 
-const store = ConfigureStore();         // st
+const store = ConfigureStore();         // store
 
 class App extends Component {           
   
